6cf69a5eed2da2e93da9d6fab0ec18dae41a0c9c
John Hayden <fogu7825@gmail.com>
2026-05-10 02:11:38 -0400

    build: BrowserRouter for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { ArticleProvider } from "./context/ArticleContext";
 import Landing from "./pages/Landing.tsx";
 import About from "./pages/About.tsx";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="/dispenser-site">
       <ArticleProvider>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </ArticleProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
